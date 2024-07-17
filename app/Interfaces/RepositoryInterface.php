d620af532b5ec12ffb4deb02357f76f878aa7c3b
Ezequiel Garibotti <ezgaribotti@gmail.com>
2024-07-17 00:12:22 -0300

    Patrón repositorio implementado

---
+++ app/Interfaces/RepositoryInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Interfaces;
+
+interface RepositoryInterface
+{
+    public function all(array $criteria = []): object;
+
+    public function create(array $attributes): object;
+
+    public function find($id): ?object;
+
+    public function findByCriteria(array $criteria): ?object;
+
+    public function findWith($id, array $relations): ?object;
+
+    public function update($id, array $attributes): void;
+
+    public function delete($id): void;
+
+    public function count(): int;
+}
