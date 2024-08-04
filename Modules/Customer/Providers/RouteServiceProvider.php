63bda7dfd397cbea10f8a7c4ef7e2802ee9eeee3
Ezequiel Garibotti <ezgaribotti@gmail.com>
2024-08-04 01:08:33 -0300

    Módulo de cliente creado

---
+++ Modules/Customer/Providers/RouteServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Modules\Customer\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    protected string $moduleNamespace = 'Modules\Customer\Http\Controllers';
+
+    public function boot(): void
+    {
+        parent::boot();
+    }
+
+    public function map(): void
+    {
+        $this->mapApiRoutes();
+
+        $this->mapWebRoutes();
+    }
+
+    protected function mapWebRoutes(): void
+    {
+        Route::middleware('web')
+            ->namespace($this->moduleNamespace)
+            ->group(module_path('Customer', '/routes/web.php'));
+    }
+
+    protected function mapApiRoutes(): void
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->moduleNamespace)
+            ->group(module_path('Customer', '/routes/api.php'));
+    }
+}
---
+++ Modules/Customer/database/seeders/CustomerDatabaseSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Customer\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class CustomerDatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
